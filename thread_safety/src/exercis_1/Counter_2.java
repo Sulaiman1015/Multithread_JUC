e480c2b7b5f6e0cf6c3c6c405d864f6341344222
Sulaiman1015 <sulaiman1015@gmail.com>
2023-12-23 22:14:29 +0100

    add files

--- thread_safety/src/exercis_1/Counter_2.java
+++ thread_safety/src/exercis_1/Counter_2.java
@@ -1,4 +1,17 @@
 package exercis_1;
 
-public class Counter_2 {
+public class Counter_2 extends Thread{
+    @Override
+    public void run() {
+        while (true){
+            synchronized (Counter_2.class){
+                if ( Center.tickets == 0){
+                    break;
+                }else {
+                    Center.tickets--;
+                    System.out.println("left "+Center.tickets+" tickets");
+                }
+            }
+        }
+    }
 }
---
+++ thread_safety/src/exercis_4/MainThread.java
@@ -0,0 +1,15 @@
+package exercis_4;
+
+public class MainThread {
+    /*
+    * share 100 dollars in 3 by 5 peoples using multi-thread
+    */
+    static int money = 100;
+    static int people = 5;
+    static int enveloppe = 3;
+
+
+    public static void main(String[] args) {
+
+    }
+}
