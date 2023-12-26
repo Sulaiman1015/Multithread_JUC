474eed7837f90150f941024c6d835ccdaa63e648
Sulaiman <72389782+Sulaiman1015@users.noreply.github.com>
2023-12-26 11:32:34 +0100

    Delete thread_base directory

--- thread_base/src/MythreadByExtends.java
+++
@@ -1,9 +0,0 @@
-public class MythreadByExtends extends Thread {
-    @Override
-    public void run() {
-        //super.run();
-        for (int i = 0; i < 5; i++) {
-            System.out.println(getName()+" -> "+i);
-        }
-    }
-}
